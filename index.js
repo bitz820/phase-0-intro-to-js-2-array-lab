85f53cf207bdfa2250b73ac30de7c700a8018081
Mark <mark.n.bacon.jr@gmail.com>
2022-03-15 12:51:04 -0400

    commit tree dfe6c6fb789919d35266300f61ffc34c70c3729a

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push('Ralph');
+    return cats
+};
+
+function destructivelyPrependCat() {
+    cats.unshift('Bob');
+    return cats
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats
+}
+
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+    return cats
+}
+
+function appendCat(){
+return [...cats, "Broom"]
+}
+
+function prependCat(){
+    return ["Arnold", ...cats]
+}
+
+function removeLastCat(){
+    let newCats = cats.slice(0, cats.length-1);
+    return newCats
+   
+}
+
+function removeFirstCat(){
+    let newCats = cats.slice(1);
+    return newCats
+}
